fe72cd406a0acd44dbb18af73da8c5cb6cf3a9d4
SkillzPlayz <130307787+SkillzPlayz@users.noreply.github.com>
2024-09-13 14:10:23 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,22 @@
 
-/*
-Online Java - IDE, Code Editor, Compiler
+class Main{
+	public static void main(String[ ] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println(“Enter your name: “);
+		String name = input.nextLine();
+		System.out.println(“Enter a verb: ");
+		String verb = input.nextLine();
+		System.out.println(“Enter an animal ")
+		String animal = input.nextLine(); 
+		System.out.println(“Enter a verb: "); 
+		
+		System.out.print("I" + verb +  " into Cheever's room.  There I saw a ");
+		System.out.print(animal + " grabbed my phone and ate it.  The only choice I had was to pull the fire alarm and suddenly ");
+		System.out.print(teacher + " stole my money and punched me! My friend " + friend + "dashed in to help and used his " + adjective + " sword to slice them up up!");
+		System.out.print(" Then we ate " + food + " while sitting on the bike stands. ");
+		System.out.print(integer + " hispanic gangsters showed up with ski masks and began yelling at me in spanish. ");
+		System.out.print(" I handed them what was leftover of my money and a " + bird + " swept down and I rode it back to cheever's class. ");
+		System.out.print(" mr. cheever pulled out his " + weapon + " and beat me up with it until I said cowabunga!! ");
+}
 
-Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
-*/
 
-import java.util.Scanner;
-class Main {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    
-  System.out.println("It was a adjective, cold november day." + "I woke up to the adjective ")}};
-  
