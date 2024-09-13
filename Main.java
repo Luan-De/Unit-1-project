6ab5b729678d4f81c2d2b3867ca40a5ce07db926
Luanmrcheeverthelegend <126310@epsstudents.org>
2024-09-13 14:06:34 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+  System.out.println("It was a adjective, cold november day." + "I woke up to the adjective ")}};
+  
