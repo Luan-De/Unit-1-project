9aed7cb48bc432152b994b10695ebbe4fc40a720
SkillzPlayz <130307787+SkillzPlayz@users.noreply.github.com>
2024-09-13 14:21:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,29 @@
-
+https://www.online-java.com/wRCt6DNa1h
 class Main{
 	public static void main(String[ ] args){
 		Scanner input = new Scanner(System.in);
-		System.out.println(“Enter your name: “);
+		System.out.println("Enter your name: ");
 		String name = input.nextLine();
-		System.out.println(“Enter a verb: ");
+		System.out.println("Enter a verb: ");
 		String verb = input.nextLine();
-		System.out.println(“Enter an animal ")
+		System.out.println("Enter an animal ")
 		String animal = input.nextLine(); 
-		System.out.println(“Enter a verb: "); 
+		System.out.println("Enter a teacher ")
+		String teacher = input.nextLine(); 
+                System.out.println("Enter a friend ")
+		String friend = input.nextLine(); 
+                System.out.println("Enter an adjective ")
+		String adjective = input.nextLine(); 
+                System.out.println("Enter a food ")
+		String food = input.nextLine(); 
+                System.out.println("Enter a integer ")
+		Int integer = input.nextInt(); 
+                System.out.println("Enter a bird ")
+		String bird = input.nextLine(); 
+                System.out.println("Enter a weapon ")
+		String weapon = input.nextLine(); 
+
+
 		
 		System.out.print("I" + verb +  " into Cheever's room.  There I saw a ");
 		System.out.print(animal + " grabbed my phone and ate it.  The only choice I had was to pull the fire alarm and suddenly ");
